67e402ee82309a834e16afcffeb50e628f384755
sjnltt3756 <sjnltt3756@gmail.com>
2025-01-11 10:41:58 +0900

    조건에 맞는 도서와 저자 리스트 출력하기

---
+++ SQL/month2/day47.sql
@@ -0,0 +1,5 @@
+SELECT B.BOOK_ID, A.AUTHOR_NAME,date_format(B.PUBLISHED_DATE,"%Y-%m-%d") as PUBLISHED_DATE
+from BOOK as B join AUTHOR as A
+on B.AUTHOR_ID = A.AUTHOR_ID
+where B.CATEGORY = "경제"
+order by PUBLISHED_DATE asc
